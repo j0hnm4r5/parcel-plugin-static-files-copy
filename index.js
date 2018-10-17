82226aa073edb560d5878d68b7fe34d51d19462d
John Mars <jmars@deeplocal.com>
2018-10-17 09:27:15 -0700

    20181017T092713

--- index.js
+++ index.js
@@ -1,43 +1,38 @@
-const fs = require("file-system");
-const path = require("path");
+const fs = require(`file-system`);
+const path = require(`path`);
 
 module.exports = (bundler) => {
-	bundler.on("bundled", (bundle) => {
-		const copyDir = (staticDir, bundleDir) => {
-			if (fs.existsSync(staticDir)) {
-				const copy = (filepath, relative, filename) => {
-					const dest = filepath.replace(staticDir, bundleDir);
-					if (!filename) {
-						fs.mkdir(filepath, dest);
-					} else if (fs.existsSync(dest)) {
-						const destStat = fs.statSync(dest);
-						const srcStat = fs.statSync(filepath);
-						if (destStat.mtime <= srcStat.mtime) {
-							console.info(
-								"Info: Static file " +
-									filepath +
-									" already exists in " +
-									bundleDir +
-									". Overwriting."
-							);
-							fs.copyFile(filepath, dest);
-						}
-					} else {
+	bundler.on(`bundled`, (bundle) => {
+		const staticDir = `assets`;
+		const bundleDir = path.dirname(bundle.name);
+
+		if (fs.existsSync(staticDir)) {
+			console.log(
+				`Found assets folder. Beginning to copy files to ./dist`
+			);
+			const copy = (filepath, relative, filename) => {
+				console.log(filepath, relative, filename);
+				const dest = filepath.replace(staticDir, bundleDir);
+				if (!filename) {
+					fs.mkdir(filepath, dest);
+				} else if (fs.existsSync(dest)) {
+					const destStat = fs.statSync(dest);
+					const srcStat = fs.statSync(filepath);
+					if (destStat.mtime <= srcStat.mtime) {
+						console.info(
+							`Info: Static file ${filepath} already exists in ${bundleDir}. Overwriting.`
+						);
 						fs.copyFile(filepath, dest);
 					}
-				};
-				fs.recurseSync(staticDir, copy);
-			} else {
-				console.warn(
-					"Warning: Static directory " +
-						staticDir +
-						" does not exist. Skipping."
-				);
-			}
-		};
-
-		const staticDir = "assets";
-		const bundleDir = path.dirname(bundle.name);
-		copyDir(staticDir, bundleDir);
+				} else {
+					fs.copyFile(filepath, dest);
+				}
+			};
+			fs.recurseSync(staticDir, copy);
+		} else {
+			console.warn(
+				`Warning: Static directory ${staticDir} does not exist. Skipping.`
+			);
+		}
 	});
 };
