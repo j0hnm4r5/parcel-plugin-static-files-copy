7d5ca01d3efd77c9b844b78415c86e0d53a06226
John Mars <jmars@deeplocal.com>
2018-10-24 15:29:14 -0400

    Remove extraneous

--- index.js
+++ index.js
@@ -2,39 +2,26 @@ const fs = require(`file-system`);
 
 module.exports = (bundler) => {
 	bundler.on(`bundled`, () => {
-		console.log(`Bundle created. Searching for assets folder.`);
+		// console.log(`Bundle created. Searching for assets folder.`);
 
 		const staticDir = `assets`;
 		const __rootName = __dirname.split(`/node_modules`)[0];
 
 		if (fs.existsSync(staticDir)) {
-			console.log(
-				`Found assets folder. Beginning to copy files to ./dist`
-			);
-			const copy = (filepath, relative, filename) => {
+			// console.log(
+			// 	`Found assets folder. Beginning to copy files to ./dist`
+			// );
+			const copy = (filepath) => {
 				const dest = filepath.replace(
 					staticDir,
 					`${__rootName}/dist/${staticDir}`
 				);
-				if (!filename) {
-					fs.mkdir(filepath, dest);
-				} else if (fs.existsSync(dest)) {
-					const destStat = fs.statSync(dest);
-					const srcStat = fs.statSync(filepath);
-					if (destStat.mtime <= srcStat.mtime) {
-						console.info(
-							`Info: Static file ${filepath} already exists in ${bundleDir}. Overwriting.`
-						);
-						fs.copyFile(filepath, dest);
-					}
-				} else {
-					fs.copyFile(filepath, dest);
-				}
+				fs.copyFile(filepath, dest);
 			};
 			fs.recurseSync(staticDir, copy);
 		} else {
 			console.warn(
-				`Warning: Static directory ${staticDir} does not exist. Skipping.`
+				`Static directory ${staticDir} does not exist.`
 			);
 		}
 	});
